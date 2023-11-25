4d7a45ea450d9b5a167635099c708b04310fc5f6
Franck Gaudin <franck@franckgaudin.com>
2023-11-25 15:38:44 -0500

    Update styles and add navigation to Root component

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,14 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 import './root.css';
 
 export default function Root() {
   return (
-    <div>
-      <h1>Layout</h1>
-      <main>
-        <Outlet />
-      </main>
+    <div className="root">
+      <div className="top-bar">
+        <h1 className="brand">Playground</h1>
+      </div>
+      <div className="wrapper">
+        <aside className="sidebar">
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <Link className="nav__link" to="/">Home</Link>
+              </li>
+              <li className="nav__item">
+                <Link className="nav__link" to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <main className="main">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
